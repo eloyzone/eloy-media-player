ca434ad3a7c68fb6b26ab52f89a5e1f4c70bc124
Elyas Hadizadeh Tasbiti <elyas_hadizadeh@hotmail.com>
2019-07-18 04:29:34 +0430

    removing a vulnerable dependency

--- src/main/java/com/github/eloymediaplayer/model/MusicMP3File.java
+++ src/main/java/com/github/eloymediaplayer/model/MusicMP3File.java
@@ -5,15 +5,6 @@ import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.UnsupportedTagException;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.TableRow;
-import org.apache.tika.exception.TikaException;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.ParseContext;
-import org.apache.tika.parser.Parser;
-import org.apache.tika.parser.mp3.Mp3Parser;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
-
 import java.io.*;
 import java.time.Duration;
 import java.util.Objects;
@@ -53,56 +44,6 @@ public class MusicMP3File
             e.printStackTrace();
         }
 
-    }
-
-    private Duration calculateDuration(File file)
-    {
-        Duration duration = null;
-        try
-        {
-            InputStream input = new FileInputStream(file);
-            ContentHandler handler = new DefaultHandler();
-            Metadata metadata = new Metadata();
-            Parser parser = new Mp3Parser();
-            ParseContext parseCtx = new ParseContext();
-            parser.parse(input, handler, metadata, parseCtx);
-            input.close();
-
-            // List all metadata
-//            String[] metadataNames = metadata.names();
-
-//            for (String name : metadataNames)
-//            {
-//                System.out.println(name + ": " + metadata.get(name));
-//            }
-
-            double durationDouble = Double.parseDouble(metadata.get("xmpDM:duration"));
-
-            duration = Duration.ofMillis((long) durationDouble);
-
-            // Retrieve the necessary info from metadata
-            // Names - title, xmpDM:artist etc. - mentioned below may differ based
-//            System.out.println("----------------------------------------------");
-//            System.out.println("Title: " + metadata.get("title"));
-//            System.out.println("Artists: " + metadata.get("xmpDM:artist"));
-//            System.out.println("Composer : " + metadata.get("xmpDM:composer"));
-//            System.out.println("Genre : " + metadata.get("xmpDM:genre"));
-//            System.out.println("Album : " + metadata.get("xmpDM:album"));
-
-        } catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        } catch (SAXException e)
-        {
-            e.printStackTrace();
-        } catch (TikaException e)
-        {
-            e.printStackTrace();
-        }
-        return duration;
     }
 
 
